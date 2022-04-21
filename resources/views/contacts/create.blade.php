62eeb1774743198df046453a8064d6f9a6daf6ab
Samuel Franca <samuelhenriques2000+alfasoft@gmail.com>
2022-04-21 13:28:45 -0300

    Add validation messages to contact creation form

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="p-6 bg-gray-300 border-b border-gray-200">
                     <form class="space-y-8 divide-y divide-gray-200" action="{{ route('contact.store') }}" method="post">
                         @csrf
                         <div class="space-y-8 divide-y divide-gray-200">
@@ -20,21 +20,48 @@
                                     <div class="sm:col-span-4">
                                         <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
                                         <div class="mt-1">
-                                            <input type="text" name="name" id="name" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
+                                            <input type="text" name="name" id="name" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md @error('name') border-red-300 text-red-900 placeholder-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500 @enderror">
+                                            @error('name')
+                                                <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                                                    <!-- Heroicon name: solid/exclamation-circle -->
+                                                    <svg class="h-5 w-5 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+                                                    </svg>
+                                                </div>
+                                                <p class="mt-2 text-sm text-red-600" id="email-error">{{ $message }}</p>
+                                            @enderror
                                         </div>
                                     </div>
 
                                     <div class="sm:col-span-4">
                                         <label for="email" class="block text-sm font-medium text-gray-700"> Email address </label>
                                         <div class="mt-1">
-                                            <input id="email" name="email" type="email" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
+                                            <input id="email" name="email" type="email" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md @error('email') border-red-300 text-red-900 placeholder-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500 @enderror">
+                                            @error('email')
+                                                <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                                                    <!-- Heroicon name: solid/exclamation-circle -->
+                                                    <svg class="h-5 w-5 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+                                                    </svg>
+                                                </div>
+                                                <p class="mt-2 text-sm text-red-600" id="email-error">{{ $message }}</p>
+                                            @enderror
                                         </div>
                                     </div>
 
-                                    <div class="sm:col-span-6">
+                                    <div class="sm:col-span-4">
                                         <label for="contact" class="block text-sm font-medium text-gray-700"> Contact number </label>
                                         <div class="mt-1">
-                                            <input type="text" name="contact" id="contact" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
+                                            <input type="text" name="contact" id="contact" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md @error('contact') border-red-300 text-red-900 placeholder-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500 @enderror">
+                                            @error('contact')
+                                                <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                                                    <!-- Heroicon name: solid/exclamation-circle -->
+                                                    <svg class="h-5 w-5 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                                        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+                                                    </svg>
+                                                </div>
+                                                <p class="mt-2 text-sm text-red-600" id="email-error">{{ $message }}</p>
+                                            @enderror
                                         </div>
                                     </div>
                                 </div>
