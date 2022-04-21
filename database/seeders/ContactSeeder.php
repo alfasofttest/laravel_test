e31e73a8a54b8849a0b56d3ac540f540dea34c66
Samuel Franca <samuelhenriques2000+alfasoft@gmail.com>
2022-04-21 11:44:16 -0300

    Implement Contact seeder and call it in DatabseSeeder

--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Contact;
 use Illuminate\Database\Seeder;
 
 class ContactSeeder extends Seeder
@@ -13,6 +14,8 @@ class ContactSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Contact::factory()
+            ->count(10)
+            ->create();
     }
 }
