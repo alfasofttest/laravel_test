2bd1c0a06e8187970f3b54c2683950f0130f7442
Samuel Franca <samuelhenriques2000+alfasoft@gmail.com>
2022-04-21 14:32:08 -0300

    Add log out button in index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,15 @@
         @if (Route::has('login'))
             <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                 @auth
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <x-dropdown-link :href="route('logout')"
+                                         onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                            {{ __('Log Out') }}
+                        </x-dropdown-link>
+                    </form>
                 @else
                     <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
